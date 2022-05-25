e769dee9df574e136f175c04c20ce6b0b3cdc91a
Jon Friend <62109917+jonnyfriendlychicago@users.noreply.github.com>
2022-05-25 18:47:38 -0500

    Assignment Complete - Ready for Review/Grading

---
+++ client/src/components/PersonCardCmp.js
@@ -0,0 +1,28 @@
+import React, { Component } from "react";
+
+class PersonCard extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      ageUp: this.props.age,
+    };
+  }
+  render() {
+    const { firstName, lastName, hairColor } = this.props;
+
+    return (
+      <div >
+        <h2>
+          {lastName}, {firstName}
+        </h2>
+        <p>Age: {this.state.ageUp}</p>
+        <p>hair Color: {hairColor}</p>
+        <button onClick={() => this.setState({ ageUp: this.state.ageUp + 1 })}>
+          Birthday Button for {firstName} {lastName}
+        </button>
+      </div>
+    );
+  }
+}
+export default PersonCard;
